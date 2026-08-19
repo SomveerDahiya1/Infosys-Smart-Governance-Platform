02b71647352ab5bd5ee08f8662803be18a712e05
Somveer Dahiya <somveerdahiya009@gmail.com>
2026-08-19 04:31:38 +0000

    Roles -Controller,Entity & Repo commit.

---
+++ backend/src/main/java/org/backend/entity/Role.java
@@ -0,0 +1,59 @@
+package org.backend.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "role_name", nullable = false, unique = true)
+    private String roleName;
+
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public Role() {
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
